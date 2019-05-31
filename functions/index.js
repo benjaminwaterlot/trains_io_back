c4bd61b086dd3b602bf6a04f4943fbbc203db5d0
Benjamin WATERLOT <waterlotbenjamin@gmail.com>
2019-05-31 18:44:15 +0200

    minor change

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,8 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const url = require('url');
 
-const getTrains = require('./src/getTrains/getTrains');
+const main = require('./src/sncf/main');
 
 admin.initializeApp({
 	credential: admin.credential.applicationDefault(),
@@ -9,10 +10,12 @@ admin.initializeApp({
 });
 
 exports.getTrains = functions.https.onRequest(async (request, response) => {
-	const result = await getTrains();
-	console.log(result);
+	// const url_parts = url.parse(request.url, true);
+	// const query = url_parts.query;
+	// console.log(query);
 
-	response.send(result);
+	const result = await main('paris', 'chambe', '06-20');
+	response.send(JSON.stringify(result));
 });
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
